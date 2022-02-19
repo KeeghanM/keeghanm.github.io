3fcc1c5d5be24305aa1a28cc19273f795f1de5ef
Keeghan McGarry <keeghan_m@live.com>
2022-02-19 17:12:31 +0000

    Initial Layout

--- components/terrain.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three'
-
-const geometry = new THREE.PlaneGeometry(100,100,25,25)
-const material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true})
-let terrainMesh = new THREE.Mesh(geometry,material)
-terrainMesh.rotateX(90)
-
-export const terrain = terrainMesh
--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-import './style.css'
-import './components/FullBackground'
+let typeString = document.querySelector(".typed").innerHTML;
+document.querySelector(".typed").innerHTML = "";
+let typeit = new TypeIt(".typed", {
+  strings: typeString,
+  speed: 25,
+  lifelike: true,
+});
+
+typeit.go();
+// console.log(typeit.getQueue().getItems());
+
+document.addEventListener("keypress", function onEvent(event) {
+  if (event.key === "Enter") {
+    typeit.destroy();
+    document.querySelector(".typed").innerHTML = typeString;
+    document.querySelector(".skip").remove();
+  }
+});
